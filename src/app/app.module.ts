e63b6141c4e08b94e92b4b2c02c26215d4769369
bgpproject <rlaalsdn567@naver.com>
2017-11-21 21:28:33 +0900

    2017.11.21

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,20 @@ import {MainComponent} from './Main/Main';
 import {LoginComponent} from './Login/Login';
 import {MenuComponent} from './Menu/Menu';
 import {FormsModule} from '@angular/forms';
+
 import {HttpClientModule} from '@angular/common/http';
 import {HttpService} from './HttpService';
+
+import {CollaborationComponent} from "./Main/Group/Collaboration/Collaboration";
+import {GroupRepositoryComponent} from "./Main/Group/GroupRepository/GroupRepository";
+import {GroupToDoComponent} from "./Main/Group/GroupToDo/GroupToDo";
+import {MemberScheduleComponent} from "./Main/Group/MemberSchedule/MemberSchedule";
+import {GroupComponent} from "./Main/Group/Group";
+import {PrivateRepositoryComponent} from "./Main/Private/PrivateRepository/PrivateRepository";
+import {PrivateScheduleComponent} from "./Main/Private/PrivateSchedule/PrivateSchedule";
+import {ProfileComponent} from "./Main/Private/Profile/Profile";
+import {PrivateComponent} from "./Main/Private/Private";
+import {SearchComponent} from "./Main/Search/Search";
 
 @NgModule({
   declarations: [
@@ -14,6 +26,16 @@ import {HttpService} from './HttpService';
     MainComponent,
     LoginComponent,
     MenuComponent,
+    CollaborationComponent,
+    GroupRepositoryComponent,
+    GroupToDoComponent,
+    MemberScheduleComponent,
+    GroupComponent,
+    PrivateRepositoryComponent,
+    PrivateScheduleComponent,
+    PrivateComponent,
+    ProfileComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
