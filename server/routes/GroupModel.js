50319764cb268be49a643bcbb62d0431d3a12dd5
Dana <koon104@naver.com>
2017-12-06 13:10:59 +0900

    groupmodel

--- server/routes/GroupModel.js
+++ server/routes/GroupModel.js
@@ -6,6 +6,11 @@ const GroupSchema = new mongoose.Schema({
   id: String,
   Files: [String],
   Members: [String],
+  Todo: [{
+    title: String,
+    content: String,
+    status: String
+  }]
 });
 
 module.exports = mongoose.model('Group', GroupSchema);
